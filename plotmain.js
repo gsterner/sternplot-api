47df00b3e735a3b69eaf39409c160d0cf3c91db3
Gustaf Sterner <gsterner@gmail.com>
2016-03-16 22:05:06 +0100

    First implementation of graph tics.

--- plotmain.js
+++ plotmain.js
@@ -50,15 +50,60 @@ function drawLine(ctx, x_values, y_values) {
     ctx.stroke();
 }
 
-function drawVerticalTic(ctx, x_value, y_value) {
-    ctx.strokeStyle = '#00f';
-    ctx.lineWidth   = 2;
+function drawSingleTicHorizontalAxis(ctx, trans, x_value, y_value) {
+    var x_trans = trans.xCoordinate(x_value);
+    var y_trans = trans.yCoordinate(y_value);
+    ctx.strokeStyle = '#708090';
     ctx.beginPath();
-    ctx.moveTo(x_value, y_value + 50);
-    ctx.lineTo(x_value,y_value - 50);
+    ctx.moveTo(x_trans, y_trans + 5);
+    ctx.lineTo(x_trans,y_trans - 5);
     ctx.stroke();
+    ctx.font="14px Arial";
+    var text_shift_away = 10 + 14;
+    var text_shift_center = 14;
+    ctx.fillText(x_value.toPrecision(4), x_trans - text_shift_center, y_trans + text_shift_away);
 }
 
+function drawTicsHorizontalAxis(ctx, trans, tic_values, axis_value) {
+    for ( i = 0; i < tic_values.length; i++) {
+        drawSingleTicHorizontalAxis(ctx, trans, tic_values[i], axis_value);
+    }    
+}
+
+function drawSingleTicVerticalAxis(ctx, trans, x_value, y_value) {
+    var x_trans = trans.xCoordinate(x_value);
+    var y_trans = trans.yCoordinate(y_value);
+    ctx.strokeStyle = '#708090';
+    ctx.beginPath();
+    ctx.moveTo(x_trans + 5, y_trans);
+    ctx.lineTo(x_trans - 5,y_trans);
+    ctx.stroke();
+    ctx.font="14px Arial";
+    var text_shift_away = 20 + 7 * 4;
+    var text_shift_center = 4;
+    ctx.fillText(y_value.toPrecision(4), x_trans - text_shift_away, y_trans + text_shift_center);
+}
+
+function drawTicsVerticalAxis(ctx, trans, tic_values, axis_value) {
+    for ( i = 0; i < tic_values.length; i++) {
+        drawSingleTicVerticalAxis(ctx, trans, axis_value, tic_values[i]);
+    }    
+}
+
+function drawTics(ctx, trans, x_array, y_array) {
+    var x_max = Numerics.getMaxOfArray(x_array);
+    var x_min = Numerics.getMinOfArray(x_array);
+    var y_max = Numerics.getMaxOfArray(y_array);
+    var y_min = Numerics.getMinOfArray(y_array);
+
+    var x_tic_step = (x_max - x_min)/5;
+    var x_tics_untransformed = range(x_min, x_max, x_tic_step);
+    var y_tic_step = (y_max - y_min)/5;
+    var y_tics_untransformed = range(y_min, y_max, y_tic_step);
+
+    drawTicsHorizontalAxis(ctx, trans, x_tics_untransformed, y_min);
+    drawTicsVerticalAxis(ctx, trans, y_tics_untransformed, x_min);
+}
 
 function drawOnCanvas(canvas, xArray, yArray) {
 //    var canvas=document.getElementById("subplot_4_4_0");
@@ -68,27 +113,16 @@ function drawOnCanvas(canvas, xArray, yArray) {
     var frame_width = canvas.width * 0.1;
     var frame_height = canvas.height * 0.1;
 
-    var x_max = Numerics.getMaxOfArray(xArray);
-    var x_min = Numerics.getMinOfArray(xArray);
-    var y_max = Numerics.getMaxOfArray(yArray);
-    var y_min = Numerics.getMinOfArray(yArray);
-
-    var x_tic_step = (x_max - x_min)/5;
-    var x_tics_untransformed = range(x_min, x_max, x_tic_step);
-
-    var x_tics = transformArrayToCoord(x_tics_untransformed, canvas_width, transformX, frame_width, frame_width)
-    var y_minmax = transformArrayToCoord([y_min, y_max], canvas_height, transformY, frame_height, frame_height)
-   
     var x_values = transformArrayToCoord(xArray, canvas_width, transformX, frame_width, frame_width)
     var y_values = transformArrayToCoord(yArray, canvas_height, transformY, frame_height, frame_height)
 
     //testing Transform refactoring
     var trans = new Transform(xArray, yArray, canvas_width, canvas_height);
-    var xtest = trans.xCoordinate(5);
-    var ytest = trans.yCoordinate(100);	
+    // var xtest = trans.xCoordinate(x_tics_untransformed[0]);
+    // var ytest = trans.yCoordinate(y_min);	
     //***	
 //    ctx.clearRect(0, 0, canvas_width, canvas_width);
-    drawVerticalTic(ctx, x_tics[2], y_minmax[0]);
+    drawTics(ctx, trans, xArray, yArray);
     drawFrame(ctx, canvas_width, canvas_height, canvas_width * 0.1, canvas_height * 0.1)
     drawLine(ctx, x_values, y_values)
 }
